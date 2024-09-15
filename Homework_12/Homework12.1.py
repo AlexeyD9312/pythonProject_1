cdf9c268ffce9b435a04f68ad5ec44b37a2bc6aa
Home <aviator171513@gmail.com>
2024-09-15 13:11:40 +0300

    Homework12.1

---
+++ Homework_12/Homework12.1.py
@@ -0,0 +1,14 @@
+#import codecs
+print("Hello")
+
+import bleach
+
+
+#with codecs.open('draft.html', 'r', 'utf-8') as file:
+ #   html = file.read()
+
+#html = '<p><span>is not <b><span>allowed</span></b></span></p>'
+#print(bleach.clean(html, tags=[], strip=True))
+
+#with codecs.open('cleaned.txt', 'w','utf-8') as f:
+  #  f.write(html)
