58634481b379a828328a5ce056b1a68b2d6d4159
Home <aviator171513@gmail.com>
2024-09-15 14:34:19 +0300

    Homework12.1

--- Homework_12/Homework12.1.py
+++ Homework_12/Homework12.1.py
@@ -1,14 +1,22 @@
 #import codecs
-print("Hello")
-
-import bleach
-
-
+#import bleach
 #with codecs.open('draft.html', 'r', 'utf-8') as file:
  #   html = file.read()
-
-#html = '<p><span>is not <b><span>allowed</span></b></span></p>'
-#print(bleach.clean(html, tags=[], strip=True))
-
+  #  html = '<p><span>is not <b><span>allowed</span></b></span></p>'
+   # print(bleach.clean(html, tags=[], strip=True))
 #with codecs.open('cleaned.txt', 'w','utf-8') as f:
-  #  f.write(html)
+ #   f.write(html)
+
+from bs4 import BeautifulSoup
+
+import codecs
+
+def delete_html_tags(html_file , result_file):
+      with codecs.open(html_file, 'r', 'utf-8') as file:
+           html = file.read()
+      soup = BeautifulSoup(html,'lxml')
+      extracted_text = soup.get_text()
+      with open(result_file, 'w', encoding='utf-8') as txt_file:
+          txt_file.write(extracted_text)
+
+delete_html_tags('draft.html', 'cleaned.txt')
