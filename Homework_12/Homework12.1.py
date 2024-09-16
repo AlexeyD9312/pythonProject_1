150133ce3a5e23b82b6591802577e318621b989e
Home <aviator171513@gmail.com>
2024-09-16 15:09:41 +0300

    Homework12.1

--- Homework_12/Homework12.1.py
+++ Homework_12/Homework12.1.py
@@ -9,8 +9,9 @@ def delete_html_tags(html_file , result_file):
            html = file.read()
       soup = BeautifulSoup(html,'lxml')
       extracted_text = soup.get_text()
+      cleaned_text = '\n'.join([line.strip() for line in extracted_text.splitlines() if line.strip()])
       with open(result_file, 'w', encoding='utf-8') as txt_file:
-          txt_file.write(extracted_text)
+          txt_file.write(cleaned_text)
 
 
 delete_html_tags('draft.html', 'cleaned.txt')
